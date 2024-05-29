b498808129dbb1528925cf95eb5c62035b0968c0
samuel rebuelta <samurebuelta@gmail.com>
2024-05-29 13:16:10 +0200

    no library form improvements

--- src/NoLibraryForm/NoLibraryForm.js
+++ src/NoLibraryForm/NoLibraryForm.js
@@ -23,7 +23,11 @@ const NoLibraryForm = () => {
     } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
       newErrors.email = 'El email no es válido';
     }
-    if (!formData.password) { newErrors.password = 'La contraseña es requerida'; }
+    if (!formData.password) {
+      newErrors.password = 'La contraseña es requerida';
+    } else if (formData.password.length < 8) {
+      newErrors.password = 'La contraseña debe tener al menos 8 caracteres';
+    }
     return newErrors;
   };
 
@@ -40,12 +44,9 @@ const NoLibraryForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const newErrors = validate();
+    setErrors(newErrors);
     if (Object.keys(newErrors).length === 0) {
-      console.log('Formulario enviado:', formData);
-      setFormData(formData);
-      setErrors({});
-    } else {
-      setErrors(newErrors);
+      console.log('Formulario enviado:', formData); 
     }
   };
 
@@ -55,16 +56,16 @@ const NoLibraryForm = () => {
       onSubmit={handleSubmit}
     >
       <input className="form__field" placeholder="Nombre" type="text" name="firstName" value={formData.firstName } onChange={handleChange } />
-      {errors.firstName && <label className="form__field--error">Este campo es obligatorio</label>}
+      {errors.firstName && <label className="form__field--error">{errors.firstName}</label>}
 
       <input className="form__field" placeholder="Apellido" type="text" name="lastName" value={formData.lastName } onChange={handleChange } />
-      {errors.lastName && <label className="form__field--error">Este campo es obligatorio</label>}
+      {errors.lastName && <label className="form__field--error">{errors.lastName}</label>}
 
       <input className="form__field" placeholder="Email" type="email" name="email" value={formData.email} onChange={handleChange} />
-      {errors.email && <label className="form__field--error">Introduce un email válido</label>}
+      {errors.email && <label className="form__field--error">{errors.email}</label>}
 
       <input className="form__field" placeholder="Contraseña" type="password" name="password" value={formData.password} onChange={handleChange} />
-      {errors.password && <label className="form__field--error">La contraseña debe tener al menos 8 caracteres</label>}
+      {errors.password && <label className="form__field--error">{errors.password}</label>}
 
       <button className="form__submit" type="submit">Registrar</button>
     </form>
